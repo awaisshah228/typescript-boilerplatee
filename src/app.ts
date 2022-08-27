9e2ea80327198455ab584815e2c91e000137ca7e
awaisshah228 <m.awaisshah228@gmail.com>
2022-08-27 13:35:46 +0500

    done

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const app = express();
 // app.use(cors({
 //   origin: 'http://localhost:3000',
 // }));
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, '..', 'public')));
